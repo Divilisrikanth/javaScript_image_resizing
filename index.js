283db29e181cd79c3b6b76bfb86971675e72969f
Divilisrikanth <srikanthdivili1994@gmail.com>
2023-04-18 17:41:23 +0530

    pushing code into repo

---
+++ index.js
@@ -0,0 +1,28 @@
+
+//let image =document.getElementsByTagName("img")[0];
+//let image1 =document.getElementsByTagName("img")[1];
+
+let image = document.getElementById("image1")
+let image1 = document.getElementById("image2")
+function increase_for_firstimage(){
+    image.width="400"
+    image.height="500"
+
+   /* image1.width="200"
+    image1.height="300"*/
+}
+function decrease_for_firstimage(){
+    image.width="200"
+    image.height="200"
+
+   /* image1.width="400"
+    image1.height="400"*/
+}
+function increase_for_secondimage(){
+    image1.width="400"
+    image1.height="500"
+}
+function decrease_for_secondimage(){
+    image1.width="200"
+    image1.height="200"
+}
